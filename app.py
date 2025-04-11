84a73b9932b5783b8d99d6c3f833e4af1a73249c
Akhilesh <akhi2431@gmail.com>
2025-04-11 18:02:03 +0100

    optimized for render

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def search():
     return jsonify(results=results)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
